4ad5e2d40b9bc5d02f66d08f19f32422c02f740f
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-10-15 21:35:11 +0900

    セットアップ完了

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// BladeOneの設定
 use eftec\bladeone\BladeOne;
+
+$views = __DIR__ . '/../src/Views';
+$cache = __DIR__ . '/../storage/cache';
+
+$blade = new BladeOne($views, $cache, BladeOne::MODE_AUTO);
+echo $blade->run("hello",array("variable1"=>"value1"));
+
+// エラー表示設定（開発中のみ）
+if (getenv('APP_DEBUG') === 'true') {
+    $blade->setMode(BladeOne::MODE_DEBUG);
+}
 
