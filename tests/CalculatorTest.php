95833ab2420d8c30329dbad9d018daff84326892
tamagoage <kokuto0512@gmail.com>
2024-10-17 19:47:11 +0900

    せっかくなので型を修正

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,13 +7,13 @@ use Tamagoage\PetDiary\Calculator;
 
 class CalculatorTest extends TestCase
 {
-    public function test_合計金額()
+    public function test_合計金額(): void
     {
         $calculator = new Calculator();
         $this->assertSame(12, $calculator->add(10, 2));
     }
 
-    public function test最初の金額から次の金額を引いた金額()
+    public function test最初の金額から次の金額を引いた金額(): void
     {
         $calculator = new Calculator();
         $this->assertSame(8, $calculator->sub(10, 2));
