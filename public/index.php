963f2aec5b60e84406acf5963aa68998fc3d2d31
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-10-21 23:37:31 +0900

    とりあえずcommitgit statusgit status

--- public/index.php
+++ public/index.php
@@ -2,7 +2,31 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Tamagoage\PetDiary\Core\Request;
+use Tamagoage\PetDiary\Core\Router;
 
 $request = new Request;
 echo 'path:  '.$request->getPath().'<br>';
 echo 'method:  '.$request->getMethod().'<br>';
+
+// $router = new Router();
+// $path1 = $router->get('/home', function(){
+//     echo "getがうまく機能している";
+// });
+
+// $path1->resolve;
+
+// リクエストオブジェクトを作成
+$request = new Request();
+
+// ルーターオブジェクトを作成
+$router = new Router($request);
+
+// ルートを定義
+$router->get('/home', function(){
+    echo "getがうまく機能している";
+});
+
+// ルートを解決
+$router->resolve();
+
+
--- src/Core/Router.php
+++ src/Core/Router.php
@@ -4,5 +4,28 @@ namespace Tamagoage\PetDiary\Core;
 
 Class Router
 {
-    
+    public function __construct
+    (
+        public Request $request,
+        public array $routes = []
+    ){}
+
+    public function get(string $path, callable $callback): void
+    {
+        $routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $method = $this->request->getMethod();
+        $path = $this->request->getPath();
+
+        $callback = $this->routes[$method][$path] ?? null;
+        // if($callback === null) {
+        //     header("HTTP/1.1 404 Not Found");
+        //     echo "404 - ないよー";
+        //     exit();
+        // }
+        call_user_func($callback);
+    }
 }
