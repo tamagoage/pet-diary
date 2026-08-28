5d766ae23b389d6a986a2fa3f67607a907287a8b
tamagoage <116415023+tamagoage@users.noreply.github.com>
2026-08-28 17:14:01 +0900

    feat: pdoの生成

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,28 @@
 <?php
+
+declare(strict_types=1);
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
+
+$db_host = getenv('DB_HOST');
+$db_port = getenv('DB_PORT');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+
+$dsn = sprintf(
+    'mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4',
+    $db_host,
+    $db_port,
+    $db_name
+);
+
+$pdo = new PDO(
+    $dsn,
+    $db_user,
+    $db_password
+);
